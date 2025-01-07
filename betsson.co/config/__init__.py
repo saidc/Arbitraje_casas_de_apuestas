c393b66f5f1df558a1312090dfd77a0ce09408eb
saidc <saidjoc@gmail.com>
2025-01-06 20:55:32 -0500

    añadiendo proyecto base con betsson.co para proyecto de Arbitraje

---
+++ betsson.co/config/__init__.py
@@ -0,0 +1,17 @@
+
+LINK_CASA_DE_APUESTA = "https://www.betsson.co/"
+APUESTAS_DEPORTIVAS = f"{LINK_CASA_DE_APUESTA}apuestas-deportivas/"
+
+DEPORTES_DE_APUESTAS = {
+    "EN VIVO": {"ID": None, "LINK": f"{APUESTAS_DEPORTIVAS}apuestas-en-vivo", "DEPORTE": "EN VIVO"},
+    "FUTBOL": {"ID": 1, "LINK": f"{APUESTAS_DEPORTIVAS}futbol", "DEPORTE": "FUTBOL"},
+    "TENIS": {"ID": 11, "LINK": f"{APUESTAS_DEPORTIVAS}tenis", "DEPORTE": "TENIS"},
+    "BASQUETBOL": {"ID": 4, "LINK": f"{APUESTAS_DEPORTIVAS}baloncesto", "DEPORTE": "BASQUETBOL"},
+    "TENIS DE MESA": {"ID": 138, "LINK": f"{APUESTAS_DEPORTIVAS}tenis-de-mesa", "DEPORTE": "TENIS DE MESA"},
+    "ESPORTS": {"ID": 119, "LINK": f"{APUESTAS_DEPORTIVAS}esports", "DEPORTE": "ESPORTS"},
+    "VOLEY": {"ID": 9, "LINK": f"{APUESTAS_DEPORTIVAS}voley", "DEPORTE": "VOLEY"},
+    "FUTBOL AMERICANO": {"ID": 10, "LINK": f"{APUESTAS_DEPORTIVAS}futbol-americano", "DEPORTE": "FUTBOL AMERICANO"},
+    "HOCKEY": {"ID": 2, "LINK": f"{APUESTAS_DEPORTIVAS}hockey-sobre-hielo", "DEPORTE": "HOCKEY"},
+    "MMA": {"ID": 53, "LINK": f"{APUESTAS_DEPORTIVAS}artes-marciales-mixtas", "DEPORTE": "MMA"},
+    "BEISBOL": {"ID": 19, "LINK": f"{APUESTAS_DEPORTIVAS}beisbol", "DEPORTE": "BEISBOL"},
+}
