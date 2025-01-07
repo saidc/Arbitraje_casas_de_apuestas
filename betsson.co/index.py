54adc3e842b65835936c6c608e1ee5543137536f
saidc <saidjoc@gmail.com>
2025-01-06 20:55:32 -0500

    añadiendo proyecto base con betsson.co para proyecto de Arbitraje

---
+++ betsson.co/index.py
@@ -0,0 +1,11 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+from config import DEPORTES_DE_APUESTAS, LINK_CASA_DE_APUESTA, APUESTAS_DEPORTIVAS
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+
+print(DEPORTES_DE_APUESTAS)
