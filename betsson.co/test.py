dd5b509b43ee46a5912a721306658bec0e0c54ae
saidc <saidjoc@gmail.com>
2025-01-18 23:48:55 -0500

    agregando un listado markettamplateIds con los nombres del tipo de apuesta obtenido de partidos_prematch.json

--- betsson.co/test.py
+++ betsson.co/test.py
@@ -1,57 +1,70 @@
-import os 
+from config import betsson_partidos_Prematch_file_path
+
 import json 
 
-# leer un archivo json llamado obtener_apuestas.json
-with open("obtener_apuestas.json", "r") as file:
-    response = json.load(file)
-
-marketIdByMarketTemplates_list = []
-
-# verificar si response contiene data 
-if "data" in response:
-    data = response["data"]
-    # verificar si data contiene widgets
-    if "widgets" in data:
-        widgets = data["widgets"]
-        # verificar si widgets es de tipo lista
-        if isinstance(widgets, list):
-            MarketList = None
-            # recorrer la lista de widgets
-            for widget in widgets:
-                # buscar el widget de tipo MarketList
-                if "type" in widget and widget["type"] == "MarketList":
-                    MarketList = widget
-                    break
-            # verificar si MarketList es distinto de None
-            if MarketList:
-                # verificar si MarketList contiene data
-                if "data" in MarketList:
-                    market_data = MarketList["data"]
-                    # verificar si market_data contiene skeleton
-                    if "skeleton" in market_data:
-                        skeleton = market_data["skeleton"]
-                        # verificar si skeleton contiene marketIdByMarketTemplates
-                        if "marketIdByMarketTemplates" in skeleton:
-                            marketIdByMarketTemplates = skeleton["marketIdByMarketTemplates"]
-                            # verificar si markets es de tipo lista
-                            if isinstance(marketIdByMarketTemplates, list):
-                                # recorrer la lista de marketIdByMarketTemplates
-                                for market in marketIdByMarketTemplates:
-                                    # verificar si market contiene marketTemplateId
-                                    marketTemplateId = market["marketTemplateId"] if "marketTemplateId" in market else None
-                                    # verificar si market contiene marketId
-                                    marketIds = market["marketIds"] if "marketIds" in market else None
-
-                                    marketIdByMarketTemplates_list.append({
-                                        "marketTemplateId": marketTemplateId,
-                                        "marketId": marketIds
-                                    })
-
-# escribir marketIdByMarketTemplates_list en un archivo llamado marketIdByMarketTemplates_list.json
-with open("marketIdByMarketTemplates_list.json", "w") as file:
-    json.dump(marketIdByMarketTemplates_list, file)
+# obtenemos el archivo json betsson_partidos_Prematch_file_path
+with open(betsson_partidos_Prematch_file_path, "r") as file:
+    betsson_prematch = json.load(file)
 
 
-                                    
+# obtener el archivo json exploracion_manual\marketTemplateIds.json 
+with open("exploracion_manual\marketTemplateIds.json", "r") as file:
+    marketTemplateIds = json.load(file)
 
+# obtener de betsson_prematch los keys 
+deportes_keys = betsson_prematch.keys()
+
+# recorrer los keys de betsson_prematch
+for deporte_key in deportes_keys:
+    # obtener el valor de la key
+    deporte = betsson_prematch[deporte_key]
+    # obtener las ligas de la key
+    ligas = deporte.keys()
+    # recorrer las ligas
+    for liga in ligas:
+        # obtener el valor de la liga
+        liga_value = deporte[liga]
+        # obtener los partidos de la liga como keys
+        partidos = liga_value.keys()
+        # recorrer los partidos
+        for partido in partidos:
+            # obtener el valor del partido
+            partido_value = liga_value[partido]
+            # verificar el partido contiene apuestas
+            if "apuestas" in partido_value:
+                apuestas = partido_value["apuestas"]
+                # obtener los tipos de apuestas como keys
+                tipos_de_apuestas = apuestas.keys()
+                # recorrer las apuestas
+                for tipo_de_apuesta in tipos_de_apuestas:
+                    # verificar si el tipo de apuesta contiene marketIds
+                    if "marketIds" in apuestas[tipo_de_apuesta]:
+                        marketIds = apuestas[tipo_de_apuesta]["marketIds"]
+                        # obtener los marketIds como keys
+                        marketIds_keys = marketIds.keys()
+                        # vericicar si el tipo de apuesta contiene marketTemplateId
+                        tipo_de_apuesta_marketTemplateId = apuestas[tipo_de_apuesta]["marketTemplateId"] if "marketTemplateId" in apuestas[tipo_de_apuesta] else None
+                        if tipo_de_apuesta_marketTemplateId:
+                            # recorrer los marketIds
+                            for marketId in marketIds_keys:
+                                # obtener el valor del marketId
+                                marketId_value = marketIds[marketId]
+                                # verificar si el marketId contiene marketFriendlyName
+                                if "marketFriendlyName" in marketId_value:
+                                    marketFriendlyName = marketId_value["marketFriendlyName"]
+                                    # convertir el texto de marketFriendlyName a texto normal ejemplo "Primer tiempo 1X2 + Ambos equipos anotar\u00e1n" a "Primer tiempo 1X2 + Ambos equipos anotarán"
+                                    #marketFriendlyName = marketFriendlyName.encode().decode("unicode_escape")
+                                    if tipo_de_apuesta_marketTemplateId not in marketTemplateIds:
+                                        # si no existe el tipo_de_apuesta_marketTemplateId en marketTemplateIds, se crea
+                                        marketTemplateIds[tipo_de_apuesta_marketTemplateId] = []
+                                    # añadir marketFriendlyName a marketTemplateIds con el valor de tipo_de_apuesta_marketTemplateId
+                                    marketTemplateIds[tipo_de_apuesta_marketTemplateId].append(marketFriendlyName)
+
+# recorrer marketTemplateIds verificando que no existan duplicados
+for marketTemplateId in marketTemplateIds:
+    marketTemplateIds[marketTemplateId] = list(set(marketTemplateIds[marketTemplateId]))
+
+# guardar marketTemplateIds en exploracion_manual\marketTemplateIds.json
+with open("exploracion_manual\marketTemplateIds.json", "w") as file:
+    json.dump(marketTemplateIds, file, indent=4)
 
