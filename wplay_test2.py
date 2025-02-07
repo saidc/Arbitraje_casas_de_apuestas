73c5057b4870a4fbd32976eb6a5b298816251c24
saidc <saidjoc@gmail.com>
2025-02-06 20:14:33 -0500

    se creo unas funciones para poder identificar si las apuestas contienen la informacion referente a una apuesta o no, de lo contratio se buscar el url_fetch que hace referencia a que se necesita hacer un request para obtener la informacion de una apuesta especifica de un partido

--- wplay_test2.py
+++ wplay_test2.py
@@ -5,6 +5,13 @@ from bs4 import BeautifulSoup
 
 Count = 0
 
+def make_request(url):
+    print("request_url: ", url)
+    payload = {}
+    headers = {}
+    response = requests.get(url, headers=headers, data=payload, timeout=10)
+    return response
+    
 def obtener_contenido_de_table(table):
     return {"table": table.name}
 
@@ -13,6 +20,41 @@ def obtener_contenido_de_div(div):
 
 def obtener_contenido_de_ul(ul):
     return {"ul": ul.name}
+
+def obtener_contenido_de_fetch_url(fetch_url):
+    global Count
+    if Count < 10:
+        Count += 1
+        respond = make_request(fetch_url)
+        if respond.status_code == 200:
+            # retornar json de la respuesta 
+            json_response = respond.json()
+            # verificar si "html" esta en el json_response
+            if "html" in json_response:
+                soup = BeautifulSoup(json_response["html"], "html.parser")
+                # obtener la primera etiqueta que aparezca en el html
+                tag = soup.find()
+                # obtener el nombre del tag
+                tag_name = tag.name
+                #print(tag)
+                tipo_de_contenido, contenido = obtener_tipo_de_tag_de_apuesta(tag_name, tag)
+                return tipo_de_contenido, contenido#, tag.text
+            return "fetch_url", json_response
+        else:
+            print(f"            ❌ Error de solicitud de fetch_url {fetch_url}")
+    return "fetch_url", "None"
+
+def obtener_tipo_de_tag_de_apuesta(tag_name, tag):
+    if tag_name == "table":
+        tipo_de_contenido = "table"
+        contenido = obtener_contenido_de_table(tag)
+    elif tag_name == "div":
+        tipo_de_contenido = "div"
+        contenido = obtener_contenido_de_div(tag)
+    elif tag_name == "ul":
+        tipo_de_contenido = "ul"
+        contenido = obtener_contenido_de_ul(tag)
+    return tipo_de_contenido, contenido
 
 # obtener informacion de apuestas de un partido
 def obtener_informacion_de_apuesta(apuesta):
@@ -35,36 +77,30 @@ def obtener_informacion_de_apuesta(apuesta):
             tags_name = [tag.name for tag in ch.children if tag.name is not None]
             if len(tags_name) > 0:
                 tag_name = tags_name[0]
-                if tag_name == "table":
-                    tipo_de_contenido = "table"
-                    contenido = obtener_contenido_de_table(ch)
-                elif tag_name == "div":
-                    tipo_de_contenido = "div"
-                    contenido = obtener_contenido_de_div(ch)
-                elif tag_name == "ul":
-                    tipo_de_contenido = "ul"
-                    contenido = obtener_contenido_de_ul(ch)
+                tipo_de_contenido, contenido = obtener_tipo_de_tag_de_apuesta(tag_name, ch)
             
-    
     if tipo_de_contenido is None and 'data-fetch_url' in apuesta.attrs:
-        fetch_url = str(apuesta['data-fetch_url'])
-        return {"id": apuesta_id, "name": apuesta_name, "fetch_url": fetch_url}
+        fetch_url = url_base + str(apuesta['data-fetch_url'])
+        tipo_de_contenido, contenido = obtener_contenido_de_fetch_url(fetch_url)
+        return {"id": apuesta_id, "name": apuesta_name, "tipo_de_contenido": tipo_de_contenido, "contenido": contenido}
     else:
         return {"id": apuesta_id, "name": apuesta_name, "tipo_de_contenido": tipo_de_contenido, "contenido": contenido}
-    
+
+def guardar_respuesta_html(response, partido_id):
+    with open(f"apuestas_{partido_id}.html", "w", encoding="utf-8") as file:
+        file.write(response.text)
+    print(f"Se ha guardado la respuesta en el archivo apuestas_{partido_id}.html")
 
 # obtener informacion de apuestas de un partido
 def solicitar_apuestas(partido_id, apuestas_url):
-    payload = {}
-    headers = {}
     
-    print("apuestas_url: ", apuestas_url)
-
     # Realizamos la peticion GET a la pagina de apuestas
-    response = requests.get(apuestas_url, headers=headers, data=payload, timeout=10)
-    
+    response = make_request(apuestas_url)
     # si el codigo de respuesta es 200 entonces hacer scraping
     if response.status_code == 200:
+        # guardar respuesta en un archivo html 
+        #guardar_respuesta_html(response, partido_id)
+
         # pasamos el texto de reaponse a un objeto BeautifulSoup
         soup = BeautifulSoup(response.text, "html.parser")
         # obtener un div con id="main-area"
@@ -102,6 +138,8 @@ def solicitar_apuestas(partido_id, apuestas_url):
     return []
 
 partido_id = 24961161
+url_base = "https://apuestas.wplay.co" #https://apuestas.wplay.co/web_nr?use_interactive_correct_score_layout=N&amp;mkt_id=624398876&amp;key=sportsbook.cms.handlers.get_mkt_content
+#              https://apuestas.wplay.co/web_nr?use_interactive_correct_score_layout=N&mkt_id=624398856&key=sportsbook.cms.handlers.get_mkt_content
 url = "https://apuestas.wplay.co/es/e/24961161/Celtic-v-Bayern-Munich"
 
 apuestas = solicitar_apuestas(partido_id, url)
